62d3a0c5af8f9e29794d4d5a2d1d89246405dc65
Arvinder singh <arvindersingh6961@gmail.com>
2024-07-07 10:44:42 -0400

    Route create for addCategory and multer logic add.

--- backend_nodejs/routes/Route.js
+++ backend_nodejs/routes/Route.js
@@ -1,9 +1,25 @@
 
 const route=require("express").Router()
+const multer=require("multer")
 const UserController=require("../controllers/UserController")
+const CategoryController=require("../controllers/CategoryController")
+
+// logic to upload images 
+const categorystorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/category')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      console.log(file)
+      cb(null, file.fieldname + '-' + uniqueSuffix+file.originalname)
+    }
+  })
+  const categoryupload = multer({ storage: categorystorage })
 
 route.post("/register",UserController)
 route.post("/login",UserController)
 route.post("/contact",UserController)
+route.post("/addcategory",categoryupload.single("image"),CategoryController)
 
 module.exports=route
