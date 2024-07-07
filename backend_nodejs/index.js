d3df126ea81bb0fcf21f3359fe9f77ec950d642a
Arvinder singh <arvindersingh6961@gmail.com>
2024-07-07 10:39:45 -0400

    Image functionality add using npm i multer

--- backend_nodejs/index.js
+++ backend_nodejs/index.js
@@ -6,6 +6,7 @@ index.use(cors())
 require("./config/Db")
 index.use(express.urlencoded({'extended':true}))
 index.use(express.json({ limit:'500mb'}))
+index.use(express.static(__dirname+"/public"))
 
 const userRoute=require("./routes/Route")
 index.use("/user",userRoute)
