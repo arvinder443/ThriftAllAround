dad20c4d4b06fa03127956213c71ffebb058ec84
Arvinder singh <arvindersingh6961@gmail.com>
2024-07-09 12:00:29 -0400

    Changes in index file and order model created.

--- backend_nodejs/index.js
+++ backend_nodejs/index.js
@@ -1,16 +1,19 @@
 
-const express=require("express")
-const index=express()
+const express = require("express")
+const index = express()
 const cors = require("cors")
 index.use(cors())
 require("./config/Db")
-index.use(express.urlencoded({'extended':true}))
-index.use(express.json({ limit:'500mb'}))
-index.use(express.static(__dirname+"/public"))
+index.use(express.urlencoded({ 'extended': true }))
+index.use(express.json({ limit: '500mb' }))
+index.use(express.static(__dirname + "/public"))
 
-const userRoute=require("./routes/Route")
-index.use("/user",userRoute)
+const seeder = require("./config/Seeder")
+seeder.adminSeeder()
 
-index.listen(3000,()=>{
+const userRoute = require("./routes/Route")
+index.use("/user", userRoute)
+
+index.listen(3000, () => {
     console.log("The port number is 3000")
 })
---
+++ backend_nodejs/models/OrderModel.js
@@ -0,0 +1,5 @@
+const mongoose=require("mongoose")
+const orderSchema=new mongoose.Schema({
+
+})
+module.exports=new mongoose.model("Order",orderSchema)
