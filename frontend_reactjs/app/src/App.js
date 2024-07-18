64984095b42151a6bed265269c024c7c880de87a
Arvinder singh <arvindersingh6961@gmail.com>
2024-07-18 09:57:25 -0400

    Material UI intergrate to the react project.

--- frontend_reactjs/app/src/App.js
+++ frontend_reactjs/app/src/App.js
@@ -1,10 +1,14 @@
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App min-h-screen bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 flex items-center justify-center">
       <header className="App-header text-white text-4xl font-bold p-10 bg-opacity-80 bg-gray-800 rounded-lg shadow-lg">
-        Hello there, I am using Tailwind CSS!
+        Hello there, I am using Tailwind CSS
+        <hr/>
+        <Button variant="contained">Contained</Button>
+
       </header>
     </div>
   );
