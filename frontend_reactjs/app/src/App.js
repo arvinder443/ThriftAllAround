5343ad3a141658492e2ec852b0d76918db3ec5c0
Arvinder singh <arvindersingh6961@gmail.com>
2024-07-26 11:27:38 -0400

    Drawer created using material ui.

--- frontend_reactjs/app/src/App.js
+++ frontend_reactjs/app/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header>
         Hello there, I am using Tailwind CSS with Titillium Web, Segoe UI, and Roboto fonts!
       </header>
---
+++ frontend_reactjs/app/src/components/NavBar.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const NavBar = () => {
+  const [state, setState] = React.useState({ left: false });
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      ((event.key === 'Tab') || (event.key === 'Shift'))
+    ) {
+      return;
+    }
+    setState({ left: open });
+  };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div className='flex items-center justify-between p-4'>
+      <div className='flex items-center space-x-4'>
+        <Button onClick={toggleDrawer(true)}>
+          <MenuIcon className='text-black' />
+        </Button>
+        <span className='text-xl font-bold text-black'>ThriftAll Around</span>
+      </div>
+      <SwipeableDrawer
+        anchor='left'
+        open={state.left}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+      >
+        {list()}
+      </SwipeableDrawer>
+    </div>
+  );
+};
+
+export default NavBar;
