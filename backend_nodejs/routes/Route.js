50cee539cf9f22fa9746f6983d99ed5a19a65827
Arvinder singh <arvindersingh6961@gmail.com>
2024-08-19 11:31:36 -0400

    Major changes in signUp and routes.

--- backend_nodejs/routes/Route.js
+++ backend_nodejs/routes/Route.js
@@ -1,28 +1,32 @@
+const route = require("express").Router();
+const multer = require("multer");
+const fs = require('fs');
+const UserController = require("../controllers/UserController");
+const CategoryController = require("../controllers/CategoryController");
 
-const route=require("express").Router()
-const multer=require("multer")
-const UserController=require("../controllers/UserController")
-const CategoryController=require("../controllers/CategoryController")
-
-// logic to upload images 
+// Logic to upload images
 const categorystorage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/category')
+      const dir = './public/category';
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+      }
+      cb(null, dir);
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      console.log(file)
-      cb(null, file.fieldname + '-' + uniqueSuffix+file.originalname)
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      console.log(file);
+      cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname);
     }
-  })
-  const categoryupload = multer({ storage: categorystorage })
+});
+const categoryupload = multer({ storage: categorystorage });
 
-route.post("/register",UserController)
-route.post("/login",UserController)
-route.post("/contact",UserController)
-route.post("/addcategory",categoryupload.single("category_image"),CategoryController.addCategory)
-route.post("/getallcategory",CategoryController.getAllCategories)
-route.post("/deletecategory",CategoryController.deleteCategory)
-route.post("/updatecategory",CategoryController.updateCategory)
+route.post("/signup", UserController.signUp);
+route.post("/login", UserController.login);
+route.post("/contact", UserController.contact); 
+route.post("/addcategory", categoryupload.single("category_image"), CategoryController.addCategory);
+route.get("/getallcategory", CategoryController.getAllCategories); 
+route.post("/deletecategory", CategoryController.deleteCategory);
+route.post("/updatecategory", CategoryController.updateCategory);
 
-module.exports=route
+module.exports = route;
